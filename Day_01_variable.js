30c1f1f354ca8bfc82d47b72ac8a3d739d44f24f
tusharbisen <tusharbisen32@gmail.com>
2024-08-18 12:26:09 +0530

    Day1 FIle added

---
+++ Day_01_variable.js
@@ -0,0 +1,13 @@
+const name = "padday";
+
+let employeeID = "10001";
+
+var  age = "89";
+
+console.table([name,employeeID,age])
+
+// console.table is use for the at a time a multiple variable Declaration Print in the Screen
+
+/*const is a one od the type of Datatype in the javscript in which suppose intialize ay value to the variable does not change the value of the variable */
+
+// in javascript we prefer the let datatype for the variable declaration beacuse of that functionl Scope and Block Scope 
