72f5ba2b94acc97bc881c7ef8307bb0bcc7e1faf
tusharbisen <tusharbisen32@gmail.com>
2024-08-18 12:39:09 +0530

    test file added

---
+++ test.js
@@ -0,0 +1,7 @@
+var id=101;
+console.log(id);
+
+
+var String  = "This is my first day beacuse before t days age i was stated the code with chai js seris so it boast as well so i promise myself i am consistant in this seris till this seris end up";
+
+console.log(String);
