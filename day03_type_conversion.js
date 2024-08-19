d59631fff8d0f2f0de582e4ff4e31e3993b2be03
tusharbisen <tusharbisen32@gmail.com>
2024-08-19 21:28:14 +0530

    type conversation in js

---
+++ day03_type_conversion.js
@@ -0,0 +1,20 @@
+// This is Type conversion 
+
+let id = 101
+console.log(id);
+
+let valuein = String(id)
+console.log(typeof(valuein));
+
+
+let num = "true" 
+
+console.log(num)
+
+let testNum = Number(num)
+console.log(testNum);
+
+console.log(typeof true);
+
+
+
