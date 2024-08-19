5cbf4c6b69ed33d4389a4fe28b580049963b44c9
tusharbisen <tusharbisen32@gmail.com>
2024-08-19 21:33:18 +0530

    demo datatyoe added  in js

---
+++ Day__datatype.js
@@ -0,0 +1,25 @@
+let Name = "I am Tushar"
+let age = 22 
+ 
+let isloggedIn = true
+
+let id = 556 
+
+let ud = null;
+
+console.log(Name);
+
+console.log(typeof 556 );
+
+// In this Lecture the most important term is null and object let discuss in further lectures 
+
+// The type of Null is Object 
+
+// we can use the typeof for the identify the typpe of variable
+
+// "Use strict " use for declare the ECMA SCRIPT and we also write a Code in the Standard Mananner 
+
+// Symbol is also one the type of Database in javascript and thhe Symbol Datatype is mailny use for the identify ur Declare the Unique 
+//   componet  in the  React JS and Symbol is also used in the Reddit or Figma as Well
+
+// Code Reaadbility is also most important term in the Coding the Code Should be Readble  for the user 
