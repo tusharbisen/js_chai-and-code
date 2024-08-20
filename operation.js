2ae51634fdb3dd50c2ac41f3945785c741d94e31
tusharbisen <tusharbisen32@gmail.com>
2024-08-20 21:02:40 +0530

    operation added

---
+++ operation.js
@@ -0,0 +1,19 @@
+// This is Operation Video why people confuse convert string to number 
+// concat the number 
+let num = "101"
+
+let num1 = 101 
+
+let num2 = num + num1
+
+console.log(num2);
+
+
+// preincrment and Postincrement 
+
+let counter = 101
+
+// ++counter;  Prefix the value increment first then print in the counter 
+
+console.log(counter);
+
