dbb19652a6a493e93ce1cececdef5a2f53573910
tusharbisen <tusharbisen32@gmail.com>
2024-08-21 22:00:56 +0530

    datatype in js added

---
+++ datatype.js
@@ -0,0 +1,59 @@
+// Comparison in js 
+
+
+console.log(2 < 2)
+
+console.log("2" == 0);   //Imprtant 
+
+console.log(null == undefined);
+
+
+console.log(null < undefined);
+
+
+// Datatype in Javascript 
+
+// primitive Datatype
+//   String Number Boolean bigint undefind Null symbol
+
+//  Non- Primitivr Datatype
+// Array Function and Object 
+
+
+//  Declaration of Primitive Datatype
+let num = 1001
+let name = "tushar"
+const signup = true
+let idnum = 10524515125n
+console.log( typeof num)
+
+const Ticket_no = null;
+
+
+//  Declaration of Non-Primitive Datatype
+
+const Fruits = ["mango","apple","pineapple","banana"];
+
+const myfunction = function( )
+{
+    console.log("This is function");
+}
+
+let myinfos = function myinf()
+{
+    Name = "Jessi"
+    id   = 101
+
+}
+
+console.log(myinfos);
+
+console.log(myfunction);
+
+console.log(typeof Ticket_no);
+
+
+
+
+
+
