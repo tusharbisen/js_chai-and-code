357edc2c7abc4ac85195257c3bf0fc3f33be3282
tusharbisen <tusharbisen32@gmail.com>
2024-08-25 22:39:49 +0530

    String in JS Added

---
+++ string.js
@@ -0,0 +1,68 @@
+// String in javascrit
+
+const name = "Tejas"
+
+const Comapany_name = "remigies"
+
+// Concatination  
+// console.log(`This is my Brother ${name} he had Completed the Graduation and recently placed at ${Comapany_name} Technology`);
+
+const repo_name = "javascript"
+
+const Repeat = "Is a Programing Laguage"
+
+console.log(repo_name.charAt([5])); 
+
+console.log(repo_name.indexOf("sp"));                             /* This is String Methods In Js */
+
+console.log(`This is javascript ${Repeat.repeat(2)}`);
+
+console.log(repo_name.split(7));
+
+console.log(repo_name.startsWith("jav"));
+ 
+console.log(Repeat.toLowerCase());
+
+console.log(Repeat.toUpperCase());
+
+console.log(Repeat.slice("3"));
+
+
+// console.log(Repeat.toLocaleLowerCase());
+
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toLowerCase());
+// Expected output: "the quick brown fox jumps over the lazy dog."
+
+
+const boat = "                This is a Indian brand and make a Earphone Headphone and Top Brand in india and the founder is aman agrawal                               " 
+
+ 
+console.log(boat) 
+// Expected output before used the trim method 
+
+console.log(boat.trim());
+// Expected output After  used the trim method 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(repo_name.length)
+
+// console.log(repo_name.toLocaleUpperCase)
+
+// const Fruit = new String("tushar")
+ 
+
+
