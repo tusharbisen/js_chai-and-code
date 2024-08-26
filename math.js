6bbe226532ced7c0304f40b307bb93d425d03b10
tusharbisen <tusharbisen32@gmail.com>
2024-08-26 22:48:19 +0530

    Number and Math Added

---
+++ math.js
@@ -0,0 +1,20 @@
+// Numbers and Math.js
+
+const  digit = new Number(1001.00)
+
+console.log(Math.abs(digit));
+
+const PageNumber = new Number(7)
+
+console.log(Math.pow(6));
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+  }
+  
+  random(1, 10);
+
+
+  const FontNumber =  98877;
+  console.log(Math.random(FontNumber))
