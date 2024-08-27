2330af9a3e727db79dd131be64d331d1c03d3720
tusharbisen <tusharbisen32@gmail.com>
2024-08-27 21:13:44 +0530

    Date and Time  Added

---
+++ date.js
@@ -0,0 +1,27 @@
+// Date and Time in Javascript 
+
+let  assigndate = new  Date();
+
+console.log(assigndate) 
+
+// In javascript Used Method For Standard Mathod
+
+console.log(assigndate.toString()
+
+console.log(assigndate.toLocaleString());
+
+let assinewdate = new Date(2024, 0, 13)
+
+console.log(assinewdate);
+
+let Manualnewdate = new Date()
+console.log(Manualnewdate.getDate());
+
+console.log(Manualnewdate.getHours());
+
+console.log(Manualnewdate.getTime());
+
+
+
+
+
