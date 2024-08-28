9485b73f193af361eb8ddd5a8fdc3e12bac8c84b
tusharbisen <tusharbisen32@gmail.com>
2024-08-28 21:45:04 +0530

    Array Part1   Added

---
+++ array_part1.js
@@ -0,0 +1,93 @@
+// Array in Javascript 
+
+// Declaration Of array 
+
+ const myarr = [1,2,3,4,5]
+
+//  console.log(myarr);
+
+ myarr.push(6)
+
+//  console.log(myarr);
+ 
+ myarr.pop()
+
+//  console.log(myarr);
+
+ const newarr = new Array(1,2,3,4,)
+
+//  console.log(newarr);
+
+//  console.log(typeof newarr);
+
+ myarr.unshift(2)
+//  console.log(myarr);
+
+ myarr.shift()
+// console.log(myarr)
+
+const fruit =  myarr.join();
+
+// console.log(fruit);
+
+// console.log(myarr);
+// console.log(typeof fruit)
+
+
+// console.log( "This is Orginal Array in Declaration ",myarr)
+
+// console.log(myarr.slice(1,3));
+
+// console.log(myarr);
+
+const mmy1 = [1,22,33,44,55,77];
+
+console.log("This is Orginal Array ",mmy1)
+
+// Used Slice Method in Javascript 
+console.log(mmy1.slice(1,3))
+
+// Used Splice Method in javascript 
+
+console.log(mmy1.splice(2,4))
+
+console.log(mmy1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+
+ 
