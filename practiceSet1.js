70c7f829f973b8f9619a181f3cbd09eeb7344f55
tusharbisen <tusharbisen32@gmail.com>
2024-09-10 12:22:00 +0530

    Pracyiceset  is  Added

---
+++ practiceSet1.js
@@ -0,0 +1,46 @@
+// To Study about all Basic concept of javscript 
+//  Array , Function , Object , Method of Array 
+
+console.log("Javascript is Programing language which include Framework Like ReactJS Nodejs");
+
+
+let Mandals = ['Lalbaghcha Raja','Dagaduset Pune','Gajanan Maharaj Shegaon'];
+
+console.log(Mandals[2]);  
+
+Mandals.push("Shivaji Mahraraj");
+
+console.log(Mandals);
+
+
+// ******************************************************************************
+
+
+const institue = {
+    id:"21BI310372",
+    name:"DKC Goregaon",
+    regi:"gonida",
+    dopening:"22/02/2012"
+}
+
+console.log(institue);
+
+ 
+let myinf = function studinfo(num1, num2){
+     
+  let result = console.log( num1 + num2);     
+  return result
+}
+myinf(10 ,20 );
+
+console.log(myinf.studinfo);
+
+
+ 
+
+
+
+
+
+
+
