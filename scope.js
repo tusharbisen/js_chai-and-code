b969f74645aa185ebe722ba3024008b2ddbbd4ad
tusharbisen <tusharbisen32@gmail.com>
2024-09-12 16:41:02 +0530

    Scope in javscipt part 2  is Added

--- scope.js
+++ scope.js
@@ -32,3 +32,52 @@ if(true)
 //  console.log(num2);
  
 //  console.log(num3);
+
+
+
+
+// ************************************** Part 2 Scope and Hoisting In javascript ************************
+
+function one()
+{
+
+  const username = "TUSHAR";
+
+   function two()
+   {
+     const website = "CHAI AND CODE";   
+     
+     console.log(username);    
+  // We can access the parent in the child scope or function 
+      
+   } 
+
+   two();
+
+// console.log(website);
+
+}
+one();
+
+// Declare the Function 
+
+addone(6);
+function addone(num)
+{
+ return num +1;
+  
+}
+
+console.log(addone(5));
+
+// We can declare a function using two method using name of the function and variable name assign to the function 
+//  const addtwo  is also known as mini Hoisting 
+
+
+const addtwo = function(num)
+{
+  return num +2 ;
+}
+
+console.log(addtwo(5));
+
