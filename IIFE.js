302ee55225cb0b4590d701c75da69af7f860f959
tusharbisen <tusharbisen32@gmail.com>
2024-09-14 21:24:30 +0530

    Immediatly invoked function expresession

---
+++ IIFE.js
@@ -0,0 +1,17 @@
+// To study the Immediatly Invoked Function Expression 
+
+ (function run()
+ {
+    console.log("I am Running on the street");
+    
+ })();
+
+//  named IIFE 
+ 
+  ((name) => {
+    console.log(`${name} i am here for u what you need just tell me i can eaisly manage the things you want ok just tell me `);
+    
+    
+  })("TUSHAR");
+  
+   
