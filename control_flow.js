a1d75199a3b274df86791f8d94d76d5efc9898e8
tusharbisen <tusharbisen32@gmail.com>
2024-09-16 21:42:54 +0530

    Control flow in javascript IF ELSE

---
+++ control_flow.js
@@ -0,0 +1,47 @@
+// To study about control flow in javascript
+// study about IF ELSE
+
+
+const userLogin = true;
+
+if(userLogin)
+{
+    console.log("Welcome to the Chai and code");
+    
+}
+
+
+const temperature = 40;
+
+if(temperature == 40)
+{
+    console.log("The Atmosphere is cloudy and hot ");
+    
+}
+else
+{
+    console.log("the temperature is noramal");
+    
+}
+ 
+
+// Nesting in loop
+
+
+const balance = 500;
+
+if(balance < 500)
+{
+    console.log("Please Deposit  the money in the account");
+    
+}
+else if(balance > 500)
+{
+    console.log("The balace is suffiecient for the account");
+    
+}
+else
+{
+    console.log("the balace is withdrwal");
+    
+}
