72db4f38211247df7c1ce91c3164e4bb4098ade1
tusharbisen <tusharbisen32@gmail.com>
2024-09-17 23:43:29 +0530

    control flow  Switch statement

---
+++ switch.js
@@ -0,0 +1,27 @@
+// Continue in Control Flow  Switch Case 
+
+const march = 4;
+
+switch (march) {
+    case 1:
+        console.log("Januray");
+        
+        break;
+    case 2:
+        console.log("Februwary");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("This is Default Case");
+        
+        break;
+}
