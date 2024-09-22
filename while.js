0d0ebe8e872367e264e55b953704f78e12d29570
tusharbisen <tusharbisen32@gmail.com>
2024-09-22 22:28:43 +0530

    while and do while loop added sucessfully

---
+++ while.js
@@ -0,0 +1,23 @@
+// In this Program we will understand while and do while loop 
+
+// creation of while loop 
+
+
+let index = 1;
+
+while(index<=10)
+{
+    console.log(`The value is ${index}`);
+    index = index + 1;
+    
+}
+
+let arr = 0;
+let Myarr = ['book','pen','forest']
+
+while(arr<=10)
+{
+    console.log(`The  values is ${Myarr}`);
+    arr =arr + 1;
+    
+}
